42b797cb5d71daafdd2c5995994c135a23f8d606
Bibash9543 <140886542+Bibash9543@users.noreply.github.com>
2024-01-10 09:58:20 +0545

    construtor_function_object_initialization

---
+++ houseKeeper_constructor_function.js
@@ -0,0 +1,10 @@
+function HouseKeeper(name, age, job){
+    this.name = name;
+    this.age = age;
+    this.job = job;
+}
+var houseKeeper1 = new HouseKeeper("Bibash", 13, ["Bathroom","living room"]);
+var houseKeeper2 = new HouseKeeper("Anjan", 17, ["Kitchen", "Kindergarden"]);
+var houseKeeper3 = new HouseKeeper("A", 12, ["Kitchen", "indgarden"]);
+var houseKeeper4 = new HouseKeeper("Anja", 15, ["Kitch", "garden"]);
+var houseKeeper5 = new HouseKeeper("Anj", 18, ["Kitcn", "Kinder"]);
